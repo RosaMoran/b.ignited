95f5d38cbdf4f5a31beea2ac6bad4e28a81cb171
Rosa Moran <rosa.moran.delgado@outlook.com>
2022-11-21 11:57:51 +0100

    adding solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please enter a number to display a number");
+int number = int.Parse(Console.ReadLine());
+while(number > 1)
+{
+    if(number % 2 == 0) // for even numbers
+        {
+            number = number / 2;
+            Console.WriteLine(number);
+        }
+        else
+        {
+            number = number * 3 + 1;
+            Console.WriteLine(number);
+        }
+}
+    return 0;
